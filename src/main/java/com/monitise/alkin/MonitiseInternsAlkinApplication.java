dd1847a53dfa32a4e5bd85248b7158b219c52020
Burak Yildirim <ceburakyildirim@gmail.com>
2017-07-10 09:48:48 +0300

    initial commit

---
+++ src/main/java/com/monitise/alkin/MonitiseInternsAlkinApplication.java
@@ -0,0 +1,12 @@
+package com.monitise.alkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitiseInternsAlkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitiseInternsAlkinApplication.class, args);
+	}
+}
